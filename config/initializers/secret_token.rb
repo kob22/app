237433c1f0ee8cc8216e2a7d17599fb88facca43
kob22 <kob2222@gmail.com>
2012-08-28 12:46:38 +0200

    edycja Gemfile, instalacja cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '698aedb4263c8e82c063bcba5ea84934cd11e011ea7f5197d434a3e5f93cfb1ed977cd027d3bab262f82b7d861d8190db49ee20d24d9ca0c5377f5c6494f4d63'
