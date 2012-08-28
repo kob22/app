69246d82a1b4eec431768af99cb3ce8ab7f81ac9
kob22 <kob2222@gmail.com>
2012-08-28 13:20:56 +0200

    dodanie rejestracji cucumber

---
+++ features/rejestracja.feature
@@ -0,0 +1,19 @@
+# language: pl
+Właściwość: Rejestracja
+
+Użytkownik może się zarejestrować, po to żeby móc bezpiecznie korzystać z serwisu.
+
+
+	Scenariusz: Poprawna rejestracja
+	Zakładając, że jesteśmy na stronie głównej
+	I klilkniemy w link Zarejestruj się
+	Wtedy zobaczymy tekst Zarejestruj się
+	I pola Tytuł, Imię, Nazwisko, Email, Hasło, Powtórz Hasło
+	I uzupełnię "Tytuł" tekstem <tytuł>
+	I uzupełnię "Imie" tekstem <imię>
+	I uzupełnię "Nazwisko" tekstem <nazwisko>
+	I uzupełnię "Email" tekstem <email>
+	I uzupełnię "Hasło" tekstem <hasło>
+	I uzupełnię "Powtórz hasło" tekstem <hasło>
+	Wtedy kliknę przycisk Zarejestruję się
+	I zobaczę tekst Rejestracja przebiegła pomylśnie
---
+++ features/step_definitions/rejestracja.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+Wtedy /^klilkniemy w link Zarejestruj się$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^zobaczymy tekst Zarejestruj się$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^pola Tytuł, Imię, Nazwisko, Email, Hasło, Powtórz Hasło$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^uzupełnię "(.*?)" tekstem <tytuł>$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^uzupełnię "(.*?)" tekstem <imię>$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^uzupełnię "(.*?)" tekstem <nazwisko>$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^uzupełnię "(.*?)" tekstem <email>$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^uzupełnię "(.*?)" tekstem <hasło>$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^kliknę przycisk Zarejestruję się$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Wtedy /^zobaczę tekst Rejestracja przebiegła pomylśnie$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
